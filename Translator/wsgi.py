2329dfca3974ce336c487e145dee0f240edea5b3
pixelThreader <162052026+piyush20B@users.noreply.github.com>
2024-04-08 22:33:26 +0530

    Update wsgi.py

--- Translator/wsgi.py
+++ Translator/wsgi.py
@@ -14,4 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Translator.settings')
 
 application = get_wsgi_application()
-app=application
+
+app = application
